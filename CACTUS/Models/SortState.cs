49bcb09d3360f3d5a0bd92b1e1a138e59f6c7003
cactusboomin <alexeiputik@yandex.by>
2020-09-05 22:30:26 +0300

    Add sort by Title and Date for collections and items.

---
+++ CACTUS/Models/SortState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CACTUS.Models
+{
+    public enum SortState
+    {
+        TitleAsc,
+        TitleDesc,
+        DateAsc,
+        DateDesc,
+    }
+}
